feb419ade9f6901133cfc524588ec7026fb1e9b7
FikriFahruRoji <fikrifahruroji@live.com>
2017-05-17 23:34:36 +0700

    Add tab layout

--- app/src/main/java/id/ncr/jamsholatapp/activities/Main.java
+++ app/src/main/java/id/ncr/jamsholatapp/activities/Main.java
@@ -1,21 +1,48 @@
 package id.ncr.jamsholatapp.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import id.ncr.jamsholatapp.R;
+import id.ncr.jamsholatapp.adapter.TabFragmentPager;
+import id.ncr.jamsholatapp.fragments.BreakFragment;
+import id.ncr.jamsholatapp.fragments.CorrectionFragment;
+import id.ncr.jamsholatapp.fragments.GeneralFragment;
+import id.ncr.jamsholatapp.fragments.InfoFragment;
 
 public class Main extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        toolbar = (Toolbar)findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(getResources().getString(R.string.app_name));
 
+        viewPager = (ViewPager)findViewById(R.id.pager);
+        tabLayout = (TabLayout)findViewById(R.id.tabs);
 
+        viewPager.setAdapter(new TabFragmentPager(getSupportFragmentManager()));
+
+        tabLayout.setTabTextColors(getResources().getColor(R.color.colorPrimaryDark),
+                getResources().getColor(android.R.color.white));
+
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
     }
-
 }
---
+++ app/src/main/java/id/ncr/jamsholatapp/adapter/TabFragmentPager.java
@@ -0,0 +1,58 @@
+package id.ncr.jamsholatapp.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import id.ncr.jamsholatapp.fragments.BreakFragment;
+import id.ncr.jamsholatapp.fragments.CorrectionFragment;
+import id.ncr.jamsholatapp.fragments.GeneralFragment;
+import id.ncr.jamsholatapp.fragments.InfoFragment;
+
+/**
+ * Created by fikri on 01/02/17.
+ */
+
+public class TabFragmentPager extends FragmentPagerAdapter {
+    //nama tab nya
+    String[] title = new String[]{"Umum", "jeda", "Koreksi", "Info Jum'at"};
+
+    public TabFragmentPager(FragmentManager fm) {
+        super(fm);
+    }
+
+    //method ini yang akan memanipulasi penampilan Fragment dilayar
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = null;
+        switch (position){
+            case 0:
+                fragment = new GeneralFragment();
+                break;
+            case 1:
+                fragment = new BreakFragment();
+                break;
+            case 2:
+                fragment = new CorrectionFragment();
+                break;
+            case 3:
+                fragment = new InfoFragment();
+                break;
+            default:
+                fragment = null;
+                break;
+        }
+
+        return fragment;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return title[position];
+    }
+
+    @Override
+    public int getCount() {
+        return title.length;
+    }
+}
